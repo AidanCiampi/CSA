969288ba1e97da19ae595f41a9f928ca06d00522
AidanCiampi <aidanciampi@billericak12.com>
2023-09-12 10:24:47 -0400

    Initial Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
-public static void main(String [] args){
-    System.out.println("Hello World");
-}
-
+    public static void main(String [] args){
+        System.out.println("Yummy");
+    chocolate();            //call the chocolate method
+    }
+    public static void chocolate (){
+        System.out.print("Chocolate ");
+        System.out.println("Cake");
+    }
 }
