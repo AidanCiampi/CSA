d07b26279db8404318abacffa0020399339828c2
AidanCiampi <Aidanciampi@billericak12.com>
2023-09-26 10:21:15 -0400

    MathEx

--- MathEx.java
+++ MathEx.java
@@ -5,8 +5,14 @@ public class MathEx {
         System.out.println(swap(592));
         timeLeft(1, 15, 4, 36);
         timeLeft(1, 34, 8, 20);
-        dayOfWeek(0,1);
-    }
+        System.out.println(dayOfWeek(0,1));
+        System.out.println(dayOfWeek(0, 14));
+        System.out.println(dayOfWeek(6, 22));
+        System.out.println(dayOfWeek(5, 4));
+        System.out.println(dayOfWeek(1, 24)); 
+        System.out.println(dayOfWeek(2, 1));
+        }
+
     public static int swap (int y){
         int onesvalue = y%10;
         int tensvalue = y%100/10;
@@ -22,11 +28,9 @@ public class MathEx {
         int waitTime = depTime - curTime;
         System.out.println(waitTime/60 + " Hours and " + waitTime%60 + " minutes");
     }
-    public static void dayOfWeek (int day, int date){
-        int 
-
-
-
+    public static int dayOfWeek (int day1, int date){
+        int day = (day1 + date - 1) % 7;
+        return day;
     }
     
 }
