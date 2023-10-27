39680e201953ff9fe70dc10685e1de7e3969e5fd
AidanCiampi <AidanCiampi@billericak23.com>
2023-10-26 22:29:45 -0400

    Classes in Classes

--- Circle.java
+++ Circle.java
@@ -1,7 +1,7 @@
 public class Circle {
     private double radius;
     public Circle(double radius){
-
+        this.radius = radius; 
     }
     public double Area(){
         return Math.PI * Math.pow(radius,2);
