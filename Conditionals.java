e2dca1bb826be1b5447d29691eb536a148361b3a
AidanCiampi <aidanciampi@billericak12.com>
2023-10-30 10:22:39 -0400

    Conditional

---
+++ Conditionals.java
@@ -0,0 +1,11 @@
+public class Conditionals {
+    
+    public boolean chimpTrouble(){
+        
+        b = chimp2
+
+    }
+    public static void main(String [] args){
+        a = chimp1;
+    }
+}
