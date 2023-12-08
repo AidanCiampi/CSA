920ba14e45402ac92eb668496e2e9b1f8976516b
AidanCiampi <aidanciampi@billericak12.com>
2023-12-08 10:21:51 -0500

    WhileLoops

--- WhileLoops.java
+++ WhileLoops.java
@@ -9,16 +9,30 @@ public class WhileLoops {
         return sum; 
     }
     public int sumDigits (int a){
-        int b = 1;
         int sum = 0;
-        while ( b < a)
-
-
+        int n;
+        while ( a > 0 ){
+            n = a%10; 
+            sum += n;   
+            a = a/10;
+        //    sum = a % 10; 
+        //    a = a+sum; 
+        //    a = a/10; 
+        }
+        return sum;
     }
+        public int howManyYears (double startpop, double endpop){
+            
+
+        }
     public static void main(String[] args){
         WhileLoops test = new WhileLoops();
-        System.out.println(test.addOdds(7));
-        System.out.println(test.addOdds(11));
-        System.out.println(test.addOdds(17));
+        // System.out.println(test.addOdds(7));
+        // System.out.println(test.addOdds(11));
+        // System.out.println(test.addOdds(17));
+        System.out.println(test.sumDigits(5765));
+        System.out.println(test.sumDigits(343));
+        System.out.println(test.sumDigits(90));
+
     }
 }
