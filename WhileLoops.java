25eb41a38b82fc7eb6eaf4b2775fc294213b790a
AidanCiampi <aidanciampi@billericak12.com>
2023-12-10 15:29:34 -0500

    WhileLoops

--- WhileLoops.java
+++ WhileLoops.java
@@ -15,24 +15,57 @@ public class WhileLoops {
             n = a%10; 
             sum += n;   
             a = a/10;
-        //    sum = a % 10; 
-        //    a = a+sum; 
-        //    a = a/10; 
         }
         return sum;
     }
         public int howManyYears (double startpop, double endpop){
-            
-
+            int years = 0;
+            while (startpop < endpop){
+                startpop *= 1.0113;
+                years = years+1; 
+            }
+            return years; 
+        }
+        public static void printSum(int n){
+            int sum = 0;
+            int i = 1;
+            while (i <= n){
+                sum = sum + i;
+                System.out.print(i);
+                if (i < n) {
+                    System.out.print( " + ");
+                }
+                i = i+1; 
+            }
+            System.out.println( " = " + sum);
+        }
+        public boolean isPerfectSquare(int n){
+            int sum = 0; 
+            int i = 1; 
+            while (sum < n){
+                sum = sum + i;
+                i = i + 2; 
+            }
+            return sum == n; 
         }
     public static void main(String[] args){
         WhileLoops test = new WhileLoops();
-        // System.out.println(test.addOdds(7));
-        // System.out.println(test.addOdds(11));
-        // System.out.println(test.addOdds(17));
+        System.out.println(test.addOdds(7));
+        System.out.println(test.addOdds(11));
+        System.out.println(test.addOdds(17));
         System.out.println(test.sumDigits(5765));
         System.out.println(test.sumDigits(343));
         System.out.println(test.sumDigits(90));
+        System.out.println(test.howManyYears(90, 120));
+        System.out.println(test.howManyYears(140,160));
+        System.out.println(test.howManyYears(180, 190)); 
+        printSum(6);
+        printSum(8);
+        System.out.println(test.isPerfectSquare(9));
+        System.out.println(test.isPerfectSquare(25));
+        System.out.println(test.isPerfectSquare(11));
+        System.out.println(test.isPerfectSquare(17));
+
 
     }
 }
