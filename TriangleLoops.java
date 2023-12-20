503687e2f4391d9149ad7d4b96217669b7db9ad4
AidanCiampi <aidanciampi@billericak12.com>
2023-12-20 10:32:51 -0500

    TriangleLoops

--- TriangleLoops.java
+++ TriangleLoops.java
@@ -1,7 +1,10 @@
 public class TriangleLoops {
     public void eights(){
         for (int i =0; i<=4; i++){
-            
+            for (int j = 8; j >=2; j=j-2){
+                System.out.print(j);
+            }
+            System.out.println();
         }
     }
     
@@ -12,5 +15,6 @@ public class TriangleLoops {
     
     public static void main(String[] args){ 
         TriangleLoops test = new TriangleLoops();
+        test.eights();
     }
 }
