3648a6ae97e3e9e3e51b5ab2a7e6b081f7a25db4
AidanCiampi <aidanciampi@billericak12.com>
2023-12-20 10:28:49 -0500

    ForLoops

---
+++ TriangleLoops.java
@@ -0,0 +1,16 @@
+public class TriangleLoops {
+    public void eights(){
+        for (int i =0; i<=4; i++){
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args){ 
+        TriangleLoops test = new TriangleLoops();
+    }
+}
