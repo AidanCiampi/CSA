2c403f6df9004fbeeb93fc6653b5288b316c328b
AidanCiampi <aidanciampi@billericak12.com>
2024-01-03 18:20:24 -0500

    AdvancedForLoops

--- AdvancedForLoops.java
+++ AdvancedForLoops.java
@@ -15,15 +15,30 @@ public class AdvancedForLoops {
         }
     }
     public static void alternate(int num){
-        for(int i = 0; i <= num; i++){ 
-            for(int j = 0; j < i; j++){ 
+        for(int i = 1; i <= num; i++){ 
+            for(int j = 1; j <= num-i; j++){ 
+                System.out.print(" ");
+            }
+            for (int j = 1; j<=i; j++){
                 System.out.print(i);
             }
-        }System.out.println();
+            for (int j = i-1; j>=1; j--){
+                System.out.print(j);
+            }
+        System.out.println();
+        }
+    }
+    public static void isosceles(int h){
+        for(int i = 1; i <= h; i++){
+            for (int j = 1; j <= i + 3; j = j+2){
+
+            }
+        }
     }
     public static void main(String[] args) {
         eights();
         alternate(6);
         alternate(5);
+        isosceles(5); 
     }
 }
