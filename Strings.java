4fdc4921612a9d2d01f173bea5338f893ecf0680
AidanCiampi <aidanciampi@billericak12.com>
2024-01-19 10:19:19 -0500

    Strings

--- Strings.java
+++ Strings.java
@@ -28,11 +28,11 @@ public class Strings{
         int month = x.lastIndexOf("/");
         x = x.replace("/", " ");
         String m = x.substring(0,day);
-        if(m.length() > 2){
+        if(m.length() < 2){
              m = "0"+m;
         }
         String d = x.substring(day+1,month);
-        if(d.length() > 2){
+        if(d.length() < 2){
             d = "0"+d;
        }
         String year = x.substring(month+1);
@@ -41,17 +41,36 @@ public class Strings{
         return date; 
 
     }
-
+    public static boolean startsWith(String s, String prefix){
+        return (s.indexOf(prefix))==0;
+    }
    
-        //     String day = x.substring(1,2) + x.substring(3,5);
-    //     day = day.replace("0","");
-    //     String month = x.substring(3,5);
-    //     month = month.replace("0","");
-    //     String year = x.substring(6,10);
-    //     String date = month + "-" + day + "-" + year;
-    //     return date;
-    // }
-        
+    public static boolean endsWith(String s, String suffix){
+        int a = suffix.length();
+        int b = s.length();
+        if(a>b){
+            return false;
+        }
+        int c = b-a;
+        String x = s.substring(c);
+        if (x.equals(suffix)){
+            return true;
+        }
+        else{
+                return false;
+            }
+        }
+    public static String removeTag(String a, String b){
+        int x = a.indexOf("<" + b + ">");
+        int y = a.indexOf("</" + b + ">");
+        if (x == -1 || y == -1){
+            return a;
+        }
+        if (x >= y){
+            return a;
+        }
+        return a.substring(x + b.length()+2, y);
+    }
     
 public static void main(String[] args) {
 System.out.println(scroll("Hello World"));
@@ -67,5 +86,27 @@ System.out.println("04/20/2014 becomes" + convertDate2("04/20/2014"));
 System.out.println("4/20/2014 becomes" + convertDate2("4/20/2014"));
 System.out.println("04/2/2014 becomes" + convertDate2("04/2/2014"));
 System.out.println("4/2/2014 becomes" + convertDate2("4/2/2014"));
+System.out.println("\nstartsWith");
+System.out.println(startsWith("architecture", "arch"));
+System.out.println(startsWith("architecture", "a"));
+System.out.println(startsWith("arch", "architecture"));
+System.out.println(startsWith("architecture", "rch"));
+System.out.println(startsWith("architecture", "architecture"));
+System.out.println("\nendsWith");
+System.out.println(endsWith("astronomy", "nomy"));
+System.out.println(endsWith("astronomy", "y"));
+System.out.println(endsWith("astronomy", "nom"));
+System.out.println(endsWith("nomy", "astronomy"));
+System.out.println(endsWith("astronomy", "astronomy"));
+System.out.println("\nremoveTag");
+System.out.println(removeTag("<b>Hello World</b>", "b"));
+System.out.println(removeTag("<b>Hello World</b>", "head"));
+System.out.println(removeTag("Hello World</b>", "b"));
+System.out.println(removeTag("<b>Hello World", "b"));
+System.out.println(removeTag("</img>Hello World<img>", "img"));
+System.out.println(removeTag("Happy Birthday <b>Hello World</b>", "b"));
+System.out.println(removeTag("<title>Hello World</title> Happy Birthday",
+"title"));
+System.out.println(removeTag("Happy <b>Hello World</b> Birthday", "b"));
     }
 }
