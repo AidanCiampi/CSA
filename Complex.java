4e462b5e01d59a28e33aae9f6e0e6417f85337c3
AidanCiampi <aidanciampi@billericak12.com>
2024-02-06 09:21:46 -0500

    Complex

---
+++ Complex.java
@@ -0,0 +1,12 @@
+public class Complex {
+    // i = square root of negative one
+    // i^2 = -1 
+    private double a;
+    private double b;
+    private i
+    
+    // part b
+    public 
+
+    
+}
